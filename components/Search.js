65daffd64e4ab2e4344ae7ef2eb7c69abf597cee
Noel Delgado <auhcsei@gmail.com>
2022-10-09 21:12:13 -0600

    refactor(components) Search: inline svg (used once)

--- components/Search.js
+++ components/Search.js
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router';
-import { IconMagnifier } from '~/components/Icons';
 import { normalizeTopicName } from '~/utils/topic';
 
 export default function Search() {
@@ -45,11 +44,14 @@ export default function Search() {
           className="icon-wrapper u-dif u-aic u-jcc"
           aria-label="Submit"
         >
-          <IconMagnifier
-            width={16}
-            height={16}
+          <svg
+            viewBox="0 0 24 24"
+            width="16"
+            height="16"
             aria-hidden="true"
-          />
+          >
+            <path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"/>
+          </svg>
         </button>
       </label>
 
