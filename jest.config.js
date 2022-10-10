b04f7dc81bbe576130eafcc94b8fe255343c39a7
Noel Delgado <auhcsei@gmail.com>
2022-10-09 18:50:39 -0600

    config: jest: add coverage options

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
@@ -13,7 +13,14 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/components/**', '<rootDir>/pages/**', '<rootDir>/utils/**'],
+  coverageThreshold: {
+    global: {
+      lines: 90
+    }
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
