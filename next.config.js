2d521514f551eefdf5993e2de51d4c09e14fdf8e
Noel Delgado <auhcsei@gmail.com>
2022-10-09 19:53:45 -0600

    feat(pages) rewrite homepage to `/topics/react`

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     locales: ['en'],
     defaultLocale: 'en'
   },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/topics/react',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
